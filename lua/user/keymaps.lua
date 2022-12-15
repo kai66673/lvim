730f15d0eb40ed835223ca9f2c4c07553fd5a1c5
Alexander Kudryavtsev <kai66673@mail.ru>
2022-12-15 18:38:16 +0300

    ...

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,6 @@
+-- set leader key
+lvim.leader = "space"
+
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.insert_mode["<C-s>"] = "<ESC>:w<cr>li"
+
---
+++ lua/user/options.lua
@@ -0,0 +1,6 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+-- vim.opt.cmdwinheight = 4
+vim.opt.cmdheight = 0
+vim.opt.showmode = true
