6c61e896b6b2376ca97744b3ade4bc6f01aaebf6
Alexander Kudryavtsev <kai66673@mail.ru>
2023-02-03 18:24:19 +0300

    ...

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,6 +1,9 @@
 -- set leader key
 lvim.leader = "space"
 
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.insert_mode["<C-s>"] = "<ESC>:w<cr>li"
+lvim.keys.normal_mode["<C-s>"] = "<CMD>w<cr>"
+lvim.keys.insert_mode["<C-s>"] = "<CMD>w<cr>"
 
+
+lvim.keys.normal_mode["<S-F12>"] = "<CMD>lua vim.lsp.buf.references()<cr>"
+lvim.keys.insert_mode["<S-F12>"] = "<CMD>lua vim.lsp.buf.references()<cr>"
